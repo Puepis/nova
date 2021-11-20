9e5a93ade0424e2f13300ac0e9405f10f82d9d7a
Philips Xu <philips.ilips@gmail.com>
2021-11-20 15:16:50 -0500

    add chakraui and routing

---
+++ src/components/pages/LoginPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box } from "@chakra-ui/react";
+
+const LoginPage = () => {
+  return (
+    <Box textAlign="center">
+      <h1>Login Page</h1>
+    </Box>
+  );
+};
+
+export default LoginPage;
---
+++ src/components/pages/NotFound.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box } from "@chakra-ui/react";
+
+const NotFound = () => {
+  return (
+    <Box textAlign="center">
+      <h1>404 Not Found 🙁</h1>
+    </Box>
+  );
+};
+
+export default NotFound;
