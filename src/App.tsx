c525544575947b3629086770e35f22cf961b0540
Isaiah Chen <58130815+chenIsai@users.noreply.github.com>
2021-11-20 16:12:20 -0500

    add color scheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NotFound from "./components/pages/NotFound";
 import LoginPage from "./components/pages/LoginPage";
@@ -7,12 +7,27 @@ import Screening from "./components/pages/Screening";
 import Overview from "./components/pages/Overview";
 import FullReview from "./components/pages/FullReview";
 
+const colors = {
+  red: "#E54D54",
+  green: "#25B718",
+  purple: {
+    100: "#E0AAFF",
+    200: "#C77DFF",
+    300: "#9D4EDD",
+    400: "#7B2CBF",
+    500: "4F2682",
+  }
+}
+
+const theme = extendTheme({ colors })
+
+
 function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Router>
         <Routes>
-          <Route path="/" element={<LoginPage />} />
+          <Route path="/" element={<Screening />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/screening" element={<Screening />} />
           <Route path="/overview" element={<Overview />} />
