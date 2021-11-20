34d6de85776913e7f46d86923b1f4cce6cf4fbc8
Philips Xu <philips.ilips@gmail.com>
2021-11-20 16:28:05 -0500

    build login page

--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -1,11 +1,49 @@
 import React from "react";
-import { Box } from "@chakra-ui/react";
+import { Flex, Image, Heading, Button } from "@chakra-ui/react";
+import LoginBackgroundImage from "../../assets/login-background.png";
+import LoginForm from "../auth/LoginForm";
 
 const LoginPage = () => {
   return (
-    <Box textAlign="center">
-      <h1>Login Page</h1>
-    </Box>
+    <Flex>
+      <Flex background="gray.100" width="40vw" height="100vh">
+        <Flex direction="column" height="100%">
+          <Flex margin="40px">
+            {/* <Image width="80px" src={Logo} alt="Nova logo" /> */}
+            <Heading as="h3" size="lg" marginLeft="40px">
+              Nova
+            </Heading>
+          </Flex>
+          <Image
+            width="100%"
+            marginTop="80px"
+            src={LoginBackgroundImage}
+            alt="Login background image"
+          />
+        </Flex>
+      </Flex>
+      <Flex
+        width="500px"
+        marginLeft="120px"
+        marginBottom="70px"
+        direction="column"
+        justifyContent="center"
+      >
+        <Heading as="h3" size="lg">
+          Sign in to continue!
+        </Heading>
+        <LoginForm />
+        <Button
+          marginTop="32px"
+          colorScheme="purple"
+          width="100%"
+          onClick={() => {}}
+          textTransform="none"
+        >
+          Login
+        </Button>
+      </Flex>
+    </Flex>
   );
 };
 
