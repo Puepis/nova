9f42d320297056180e116a0462add2e748893baa
Isaiah Chen <58130815+chenIsai@users.noreply.github.com>
2021-11-20 16:27:47 -0500

    add header and screening basics

---
+++ src/components/header.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Flex, Button, Text } from '@chakra-ui/react';
+
+const Header = () => {
+  return (
+    <>
+      <Flex position="fixed" w="100%">
+        <Text margin="8px 0px 0px 16px" fontFamily="inter" fontWeight="bold" fontSize="30px">
+          Nova
+        </Text>
+        <Button margin="8px 16px 0px auto" backgroundColor="purple.300" color="white">
+          Logout
+        </Button>
+      </Flex>
+    </>
+  )
+}
+
+export default Header
--- src/components/pages/Screening.tsx
+++ src/components/pages/Screening.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import { Box, Flex, Tabs, TabList, Tab } from '@chakra-ui/react';
+import Header from '../header'
 
 const Screening = () => {
   return (
-    <div>WIP</div>
+    <Box height="100vh">
+      <Header />
+      <Flex>
+        <Tabs>
+          <TabList>
+            <Tab>Test</Tab>
+            <Tab>Test2</Tab>
+            <Tab>Test3</Tab>
+          </TabList>
+        </Tabs>
+      </Flex>
+    </Box>
   )
 }
 
